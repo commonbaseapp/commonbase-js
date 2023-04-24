52aa4acc208b9b8043bf3e051a4069b5efeb96e9
Christoph Witzko <github@christophwitzko.com>
2023-04-24 12:49:32 +0200

    feat: add truncation example

---
+++ examples/variable-truncation.ts
@@ -0,0 +1,34 @@
+import { readFile } from "fs/promises";
+import * as process from "process";
+
+// import { Client } from "@commonbase/sdk";
+import { Client } from "../index";
+
+async function main() {
+  const client = new Client({
+    projectId: process.env.CB_PROJECT_ID,
+  });
+
+  const buildLogs = await readFile("./build.log", "utf8");
+  const completionStream = await client.createStreamingCompletion({
+    variables: {
+      build_logs: buildLogs,
+    },
+    truncateVariable: {
+      strategy: "truncate_head",
+      maxPromptTokens: 3000,
+    },
+  });
+  for await (const completionResult of completionStream) {
+    if (completionResult.completed) {
+      process.stdout.write("\n\n");
+      console.log("streamCompleted", completionResult);
+      continue;
+    }
+    process.stdout.write(completionResult.bestResult);
+  }
+
+  console.log("done");
+}
+
+main().catch(console.error);
