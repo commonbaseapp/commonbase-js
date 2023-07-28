a0b8a0a827e4555b1e19a2a6a1c28c5c6a918cee
Brandon Meyerowitz <brandon@commonbase.com>
2023-07-28 10:17:00 +0200

    simplify mock response generator

--- client/stream-consumer.test.ts
+++ client/stream-consumer.test.ts
@@ -4,44 +4,35 @@ import { CompletionResult } from "./completion-result";
 import { StreamConsumer } from "./stream-consumer";
 import type { CompletionResponse } from "./types";
 
-const mockCompletionResponseText = `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu posuere justo. 
-  Donec vel facilisis velit, posuere molestie sapien. Etiam sollicitudin est sed 
-  massa accumsan accumsan. Vivamus in nunc at tellus rhoncus cursus. Aenean nisi 
-  est, egestas eu lorem ut, mattis feugiat lacus. Praesent nisl nibh, gravida 
-  mollis ipsum et, tempor volutpat nulla.`;
+const mockCompletionResponseText = `Lorem ipsum dolor sit amet, consectetur 
+  adipiscing elit. Cras eu posuere justo. Donec vel facilisis velit, posuere
+  molestie sapien. Etiam sollicitudin est sed massa accumsan accumsan. Vivamus 
+  in nunc at tellus rhoncus cursus. Aenean nisi est, egestas eu lorem ut, 
+  mattis feugiat lacus. Praesent nisl nibh, gravida mollis ipsum et, tempor 
+  volutpat nulla.`;
 
 /**
- * Splits mockCompletionResponseText into randomly-sized chunks, then composes
- * a CompletionResponse object for each of these chunks.
+ * Splits mockCompletionResponseText into word chunks, then composes a
+ * CompletionResponse object for each of these chunks.
  *
  * These responses are then encoded and streamed through a ReadableStream in the tests.
  */
-function getMockCompletionResponses(): CompletionResponse[] {
-  const responses: CompletionResponse[] = [];
-  for (let chunkStart = 0; chunkStart < mockCompletionResponseText.length; ) {
-    const chunkSize = Math.floor(Math.random() * 16);
-    const chunkEnd = Math.min(
-      chunkStart + chunkSize,
-      mockCompletionResponseText.length,
-    );
-    responses.push({
-      completed: false,
-      invocationId: "",
-      projectId: "",
-      type: "chat",
-      model: "",
-      choices: [
-        {
-          index: 0,
-          finish_reason:
-            chunkEnd === mockCompletionResponseText.length ? "stop" : null,
-          text: mockCompletionResponseText.slice(chunkStart, chunkEnd),
-        },
-      ],
-    });
-    chunkStart = chunkEnd;
-  }
+export function getMockCompletionResponses(): CompletionResponse[] {
+  const words = mockCompletionResponseText.split(" ");
+  const responses = words.map((word, i) => ({
+    completed: false,
+    invocationId: "",
+    projectId: "",
+    type: "chat",
+    model: "",
+    choices: [
+      {
+        index: 0,
+        finish_reason: i === words.length - 1 ? "stop" : null,
+        text: i === words.length - 1 ? word : `${word} `,
+      },
+    ],
+  }));
   return [
     ...responses,
     // The API sends down one last response that contains the full completion string.
@@ -62,7 +53,7 @@ function getMockCompletionResponses(): CompletionResponse[] {
   ];
 }
 
-const mockResponses = getMockCompletionResponses();
+const mockResponses: CompletionResponse[] = getMockCompletionResponses();
 
 describe("StreamConsumer", () => {
   it("correctly parse stream data", async () => {
